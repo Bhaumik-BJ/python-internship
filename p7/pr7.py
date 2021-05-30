88bfc57553d6300b6cdb306657edc8fd25806b9d
Bhaumik <82642733+Bhaumik-BJ@users.noreply.github.com>
2021-05-30 22:16:17 +0530

    Delete p7 directory

--- p7/pr7.py
+++
@@ -1,9 +0,0 @@
-x = int(input("Enter the Number1:"))
-y = int(input("Enter the Number2:"))
-
-temp = x
-x = y
-y = temp
-
-print('The value of x after swapping:',x)
-print('The value of y after swapping:',y)
